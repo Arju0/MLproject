173c8d00d29abc7d581c0925314758d65974f894
Arju <arjufaujdar23@gmail.com>
2025-01-30 11:39:56 +0530

    commit

--- src/utils.py
+++ src/utils.py
@@ -1,93 +1,62 @@
-import sys
-from typing import Dict, Tuple
 import os
+import sys
+
+import numpy as np 
 import pandas as pd
+import dill
 import pickle
-import yaml
-import boto3
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
-
-from src.constant import *
 from src.exception import CustomException
-from src.logger import logging
 
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
 
+        os.makedirs(dir_path, exist_ok=True)
 
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
-class MainUtils:
-    def __init__(self) -> None:
-        pass
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
 
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-    def read_yaml_file(self, filename: str) -> dict:
-        try:
-            with open(filename, "rb") as yaml_file:
-                return yaml.safe_load(yaml_file)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-        except Exception as e:
-            raise CustomException(e, sys) from e
+            #model.fit(X_train, y_train)  # Train model
 
+            y_train_pred = model.predict(X_train)
 
-    def read_schema_config_file(self) -> dict:
-        try:
-            schema_config = self.read_yaml_file(os.path.join("config", "schema.yaml"))
+            y_test_pred = model.predict(X_test)
 
+            train_model_score = r2_score(y_train, y_train_pred)
 
-            return schema_config
+            test_model_score = r2_score(y_test, y_test_pred)
 
+            report[list(models.keys())[i]] = test_model_score
 
-        except Exception as e:
-            raise CustomException(e, sys) from e
+        return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
-   
-
-
-    @staticmethod
-    def save_object(file_path: str, obj: object) -> None:
-        logging.info("Entered the save_object method of MainUtils class")
-
-
-        try:
-            with open(file_path, "wb") as file_obj:
-                pickle.dump(obj, file_obj)
-
-
-            logging.info("Exited the save_object method of MainUtils class")
-
-
-        except Exception as e:
-            raise CustomException(e, sys) from e
-
-
-   
-
-
-    @staticmethod
-    def load_object(file_path: str) -> object:
-        logging.info("Entered the load_object method of MainUtils class")
-
-
-        try:
-            with open(file_path, "rb") as file_obj:
-                obj = pickle.load(file_obj)
-
-
-            logging.info("Exited the load_object method of MainUtils class")
-
-
-            return obj
-
-
-        except Exception as e:
-            raise CustomException(e, sys) from e
-   
-    @staticmethod    
-    def load_object(file_path):
-        try:
-            with open(file_path,'rb') as file_obj:
-                return pickle.load(file_obj)
-        except Exception as e:
-            logging.info('Exception Occured in load_object function utils')
-            raise CustomException(e,sys)
+    except Exception as e:
+        raise CustomException(e, sys)
